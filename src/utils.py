b37f3617260080f97b58657684fcddc2e84d0465
Georgiy <petrosangosa@gmail.com>
2026-06-01 16:50:51 +0300

    add pydantic add config test fix config_schema and utils and test config

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,9 @@
-from config_shema import AppConfig
 import yaml
 
+from src.config_schema import AppConfig
 
-def laod_config(config_path: str) -> AppConfig:
+
+def load_config(config_path: str) -> AppConfig:
     with open(config_path, "r", encoding="utf-8") as f:
         config_dict = yaml.safe_load(f)
 
