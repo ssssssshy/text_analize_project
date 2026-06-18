008baadd74f7db8295723d908613351dae3a4165
Georgiy <petrosangosa@gmail.com>
2026-06-18 12:25:37 +0300

    Refactor model training and prediction scripts; remove unused classes and files
    - Deleted tokenizer configuration file as it is no longer needed.
    - Refactored model configuration schema to remove logistic regression and TF-IDF configurations.
    - Removed SimpleTransformerClassifier implementation and replaced it with MyModelV2 in prediction scripts.
    - Updated prediction logic to use AutoModelForSequenceClassification for BERT.
    - Removed training script for SimpleTransformerClassifier and TF-IDF + Logistic Regression model.
    - Adjusted paths for model weights and updated logging for predictions.

--- src/config_schema.py
+++ src/config_schema.py
@@ -13,23 +13,11 @@ class ModelConfig(BaseModel):
     num_classes: int
 
 
-class ModelLogisticRegressionConfig(BaseModel):
-    C: float
-    max_iter: int
-    class_weight: str
-    random_state: int
-
-
-class ModelSimpleTransformerConfig(BaseModel):
-    d_model: int
-    nhead: int
+class mymodelv2_params(BaseModel):
+    embedding_dim: int
+    num_heads: int
     num_layers: int
     num_classes: int
-
-
-class ModelTfidfConfig(BaseModel):
-    max_features: int
-    stop_words: str
 
 
 class TrainingConfig(BaseModel):
@@ -39,18 +27,8 @@ class TrainingConfig(BaseModel):
     learning_rate: float
 
 
-class mymodelv2_params(BaseModel):
-    embedding_dim: int
-    num_heads: int
-    num_layers: int
-    num_classes: int
-
-
 class AppConfig(BaseModel):
     data: Config
     model: ModelConfig
     training: TrainingConfig
-    tfidf: ModelTfidfConfig
-    logistic_regression: ModelLogisticRegressionConfig
-    my_model_params: ModelSimpleTransformerConfig
     mymodelv2_params: mymodelv2_params
--- src/predict.py
+++ src/predict.py
@@ -1,14 +1,9 @@
-import argparse
 import os
-import sys
 import warnings
-
-import joblib
 import sklearn.exceptions
 import torch
-from transformers import AutoModelForSequenceClassification, AutoTokenizer
 
-from src.model.mymodel import SimpleTransformerClassifier
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from src.model.mymodelv2 import MyModelV2
 
 warnings.filterwarnings(
@@ -18,7 +13,6 @@ warnings.filterwarnings(
 
 
 def predict_mymodelv2(text, model_path):
-
     device = (
         torch.accelerator.current_accelerator.type()
         if torch.accelerator.is_available()
@@ -29,7 +23,7 @@ def predict_mymodelv2(text, model_path):
 
     model = MyModelV2(tokenizer=tokenizer)
 
-    weight_path = os.path.join(model_path, "models/custom_modelv2/model_weight.pth")
+    weight_path = os.path.join(model_path, "model_weights.pth")
     model.load_state_dict(torch.load(weight_path, map_location=device))
     model.to(device)
     model.eval()
@@ -39,19 +33,18 @@ def predict_mymodelv2(text, model_path):
     )
 
     input_ids = inputs["input_ids"].to(device)
-    attention_mask = inputs["attention_mask"].to(device)
+    attention_mask = inputs["attention_mask"].to(device).bool()
 
     with torch.no_grad():
         logits = model(input_ids, attention_mask=attention_mask)
-
         probs = torch.softmax(logits, dim=1)
-
         pred_class = int(torch.argmax(probs, dim=1).item())
         confidence = probs[0][pred_class].item()
+
     return pred_class, confidence
 
 
-def predict_mymodel(text, model_path):
+def predict_bert(text, model_path):
     device = (
         torch.accelerator.current_accelerator.type()
         if torch.accelerator.is_available()
@@ -60,41 +53,40 @@ def predict_mymodel(text, model_path):
 
     tokenizer = AutoTokenizer.from_pretrained(model_path)
 
-    model = SimpleTransformerClassifier(tokenizer)
-    weigth_path = os.path.join(
-        model_path, "models/custom_transformers_classification/model_weight.pth"
-    )
-    model.load_state_dict(torch.load(weigth_path, map_location=device))
+    model = AutoModelForSequenceClassification.from_pretrained(model_path)
+
     model.to(device)
     model.eval()
 
     inputs = tokenizer(
-        text, return_tensors="pt", truncation=True, padding=True, max_length=128
+        text, return_tensors="pt", truncation=True, padding=True, max_lenght=128
     )
 
     input_ids = inputs["input_ids"].to(device)
     attention_mask = inputs["attention_mask"].to(device)
 
     with torch.no_grad():
-        logits = model(input_ids, attention_mask=attention_mask)
+        outputs = model(input_ids, attention_mask=attention_mask)
+        logits = outputs.logits
         probs = torch.softmax(logits, dim=1)
         pred_class = int(torch.argmax(probs, dim=1).item())
         confidence = probs[0][pred_class].item()
 
-    return pred_class, confidence
+        return pred_class, confidence
 
 
 def main():
-    model_path = "./models"
     text = "Очень плохой фильм"
 
-    print("Тестируем MyModelV2")
-    res_class, confidence = predict_mymodelv2(text, model_path)
-    print(f"Результат V2: Класс {res_class}, Уверенность: {confidence:.4f}")
+    print("Тестируем MyModelV2...")
+    model_v2_path = "./models/custom_modelv2"
+    res_class, confidence = predict_mymodelv2(text, model_v2_path)
+    print(f"Результат V2: Класс {res_class}, Уверенность: {confidence:.4f}\n")
 
-    print("Тестируем MyModel")
-    res_class, confidence = predict_mymodel(text, model_path)
-    print(f"Результат V1: Класс {res_class}, Уверенность: {confidence:.4f}")
+    print("Тестируем Отфатюниный BERT...")
+    model_bert_path = "./models/bert_sequence_classification"
+    res_class, confidence = predict_bert(text, model_bert_path)
+    print(f"Результат V2: Класс {res_class}, Уверенность: {confidence:.4f}\n")
 
 
 if __name__ == "__main__":
