34111ab412a1c25be74c6ea706840a5b5960dc9c
Georgiy <petrosangosa@gmail.com>
2026-07-06 12:48:11 +0300

    Fix dataset path in test_load_data function

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -3,7 +3,7 @@ from src.dataset import load_data
 
 def test_load_data():
     X_train, X_val, X_test, y_train, y_val, y_test = load_data(
-        "data/processed/cleaned_dataset.csvv"
+        "data/processed/cleaned_dataset.csv"
     )
     assert len(X_train) > 0
     assert len(X_val) > 0
